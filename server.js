c24adb9965eb0ed55d3d0c4a7e42ce3acd7440f5
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-11 00:07:05 +0500

    fix: correct dotenv configuration and add port definition; create db.js file

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const dotenv = require('dotenv')
-dotenv.config
+dotenv.config()
+const port = process.env.PORT || 5000
+
 const app = express()
-const port = process.env.PORT
 
 app.listen(port, (req, res) =>{
     console.log(`Server is running at ${port}`)
