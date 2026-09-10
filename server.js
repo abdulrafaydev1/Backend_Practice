412d99fcbbbb73962d0d05fa1e7a728b680e5e7b
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-11 00:33:51 +0500

    feat: add product routes and controller; update server.js to include product routing

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv')
-const dbConnected = require('./config/db')
+const product = require('./routes/product')
 dotenv.config()
 const port = process.env.PORT || 5000
 dbConnected()
 
 const app = express()
 
+app.use('/product', product)
+ 
 app.listen(port, (req, res) =>{
     console.log(`Server is running at ${port}`)
 })
